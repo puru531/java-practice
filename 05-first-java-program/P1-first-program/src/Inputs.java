5ff23ebb4358b1279aeb65cbcfef8423d69b6320
Purushottam Kumar <purushottam1619@gmail.com>
2024-07-05 02:15:44 +0530

    first program

---
+++ 05-first-java-program/P1-first-program/src/Inputs.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Inputs {
+    public static void main(String[] args) {
+
+        //taking input in java
+        Scanner sc = new Scanner(System.in);
+        int rollno = sc.nextInt();
+        System.out.println("Your roll number is : " + rollno);
+    }
+}
---
+++ 05-first-java-program/P1-first-program/src/Primitives.java
@@ -0,0 +1,21 @@
+public class Primitives {
+    public static void main(String[] args) {
+        //Primitives : any data type which you cannot break even further.
+        int rollno = 64;
+        char letter = 'p';
+        float marks = 98.67f; //all decimals are by default double, in order to get more accurate value of decimal, we use float, which is denoted by adding f at the last of the decimal number.
+        double largeDecimalNumbers = 456466.6546;
+        long largerIntegerValue = 465465465465655354L;
+        boolean check = true;
+
+
+        //Each primitive data types have their classes also:
+        Integer rno = 64;
+
+
+
+
+        //String is not primitive
+        String  name = "Purushottam";
+    }
+}
