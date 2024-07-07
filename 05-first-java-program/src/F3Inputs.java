539a8c13812aa9304b5641f264d1c3f412c3989a
Purushottam Kumar <purushottam1619@gmail.com>
2024-07-08 00:38:46 +0530

    file structure

---
+++ 05-first-java-program/src/F3Inputs.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class F3Inputs {
+    public static void main(String[] args) {
+
+        //taking input in java
+        Scanner sc = new Scanner(System.in);
+        System.out.print("please enter a number : ");
+        int rollno = sc.nextInt();
+        System.out.println("Your roll number is : " + rollno);
+
+
+//        String name = sc.next(); // take a word as input
+//        String sen = sc.nextLine(); // takes whole line as input
+//        float num = sc.nextFloat(); // if int is passed in input, it will be converted to flaot, because float is greater than int.
+    }
+}
---
+++ 05-first-java-program/src/F4TypeCasting.java
@@ -0,0 +1,5 @@
+public class F4TypeCasting {
+    public static void main(String[] args) {
+        //Type Conversion anf Casting
+    }
+}
